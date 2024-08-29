717319471b060cd73d2b6b2a7acd449751fdb031
javaManiak <138834749+javaManiak@users.noreply.github.com>
2024-08-28 23:42:48 -0500

    first commit

---
+++ src/main/java/com/myapp/myapp/MyController.java
@@ -0,0 +1,13 @@
+package com.myapp.myapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class MyController {
+
+    @RequestMapping("/api/health")
+    public String sayHello() {
+        return "API health is good";
+    }
+}
