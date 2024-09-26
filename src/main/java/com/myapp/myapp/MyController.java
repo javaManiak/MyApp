cc1c1d490e859c03dd285aa6caa8ccd9aba6a68b
javaManiak <138834749+javaManiak@users.noreply.github.com>
2024-09-26 01:37:51 -0500

    make it sooo sooo good

--- src/main/java/com/myapp/myapp/MyController.java
+++ src/main/java/com/myapp/myapp/MyController.java
@@ -8,6 +8,6 @@ public class MyController {
 
     @RequestMapping("/api/health")
     public String sayHello() {
-        return "API health is SOO.. good";
+        return "API health is SOO......Soooo... good";
     }
 }
